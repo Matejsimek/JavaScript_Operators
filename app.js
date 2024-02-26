dfb2ebdf2699bf809b7ad375c8dfa88cd09e8e4d
Matej Simek <matisis135@gmail.com>
2024-02-26 12:46:50 -0500

    work done

--- app.js
+++ app.js
@@ -6,14 +6,55 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+var a = 20;
+
+var b = 4;
+
+var add = a + b;
+
+var minus = a - b;
+
+var multiply = a * b;
+
+var dividing = a / b;
+
+console.log(`${a} + ${b} = ${add}`);
+console.log(`${a} - ${b} = ${minus}`);
+console.log(`${a} * ${b} = ${multiply}`);
+console.log(`${a} / ${b} = ${dividing}`);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(`What is the value of: num + str? Answer: ${num + str}`);
+console.log(`What is the value of: num + str2? Answer: ${num + str2}`);
+console.log(`What is the value of: num + isPresent? Answer: ${num + isPresent}`);
+console.log(`What is the value of: firstName + num? Answer: ${firstName + num}`);
+console.log(`What is the value of: isPresent + str? Answer: ${isPresent + str}`);
+console.log(`What is the value of: firstName + lastName? Answer: ${firstName + lastName}`);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(`What is the value of: val == str3? Answer: ${val == str3} `)
+console.log(`What is the value of: val === str3? Answer: ${val === str3}`)
+console.log(`What is the value of: !isAwake? Answer: ${!isAwake}`)
+console.log(`What is the value of: ("eleven" == str4 && val >= str3)? Answer: ${("eleven" == str4 && val >= str3)}`)
+console.log(`What is the value of: (!isAwake || isAwake)? Answer: ${ (!isAwake || isAwake)}`)
+console.log(`What is the value of: 0 == false? Answer: ${0 == false}`)
+console.log(`What is the value of: 0 === false? Answer: ${0 === false}`)
+console.log(`What is the value of: 0 != false? Answer: ${0 != false}`)
+console.log(`What is the value of: 0 !== false? Answer: ${0 !== false}`)
+
